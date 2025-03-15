c2e6e61d02506cbaa7e8b35f92f63a0a97e98390
Arka-SSD <mohamadkheiry75@gmail.com>
2025-03-15 11:34:03 +0330

    *

--- src/sections/Blogs.stories.tsx
+++ src/sections/Blogs.stories.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
 import { Blogs } from './Blogs'
 import BG from '../../.storybook/public/image-post1.webp'
-import { ImageTest } from '../tests/payload'
-
+import { ImageTest, BlocksTest, HeroTest, RichTextTest } from '../tests/payload'
 const sampleBlogs = [
   {
     title: 'INSIGHT',
@@ -14,24 +13,38 @@ const sampleBlogs = [
   },
   {
     title: 'NEWS',
-    content: 'Business Outcomes of an AI Centre of Excellence',
+    content: 'Business Outcoes of an AI Centre of Excellence',
     link: { label: 'LEARN MORE', url: '#' },
     image: ImageTest.hero,
     name: '2',
   },
   {
     title: 'INSIGHT',
-    content: 'AI & Business: The Next Big Thing in Tech',
+    content: 'Unleashing the Power of AI: Building Foundations for Success',
     link: { label: 'LEARN MORE', url: '#' },
     image: ImageTest.hero,
     name: '3',
   },
   {
     title: 'NEWS',
-    content: 'Business Outcomes of an AI Centre of Excellence',
+    content: 'Business Outcoes of an AI Centre of Excellence',
     link: { label: 'LEARN MORE', url: '#' },
     image: ImageTest.hero,
     name: '4',
+  },
+  {
+    title: 'INSIGHT',
+    content: 'Unleashing the Power of AI: Building Foundations for Success',
+    link: { label: 'LEARN MORE', url: '#' },
+    image: ImageTest.hero,
+    name: '5',
+  },
+  {
+    title: 'NEWS',
+    content: 'Business Outcoes of an AI Centre of Excellence',
+    link: { label: 'LEARN MORE', url: '#' },
+    image: ImageTest.hero,
+    name: '6',
   },
 ]
 
@@ -54,5 +67,5 @@ export default meta
 type Story = StoryObj<typeof Blogs>
 
 export const Default: Story = {
-  render: (args) => <Blogs {...args} />,
+  render: (args) => <Blogs title="Latest News And Insights" bgImage={BG} blogs={sampleBlogs} />,
 }
