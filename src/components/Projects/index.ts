6e5a29a9b5fd49a5a03382c347498508bb97fe3a
Rob Ellison <r.ellison@reply.com>
2025-08-31 17:16:18 +0000

    fix: export views

--- src/components/Projects/index.ts
+++ src/components/Projects/index.ts
@@ -1,1 +1,7 @@
+
 export { ProjectCard as DCProjectCard } from "./project-card"
+export * from "./KanbanBoardView"
+export * from "./PlanningView"
+export * from "./TasksView"
+export * from "./FileView"
+export * from "./EpicsView"
