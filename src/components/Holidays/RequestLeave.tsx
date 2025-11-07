da3dfe3af98bc406c6c109e5ecc4d6cea7c3ad1c
Mark Pedley <m.pedley@reply.com>
2025-11-07 09:52:24 +0000

    revert mistake

--- src/components/Holidays/RequestLeave.tsx
+++ src/components/Holidays/RequestLeave.tsx
@@ -76,15 +76,11 @@ export function RequestLeave({ remainingDays, submitLeaveRequest }: RequestLeave
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
-
-    const startYear = startDate?.getUTCFullYear()
-    const endYear = endDate?.getUTCFullYear()
-
     if (!startDate || !endDate) {
       toast.error('Please select both start and end dates')
       return
     }
-    if (startYear == endYear && totalDays > remainingDays) {
+    if (totalDays > remainingDays) {
       toast.error('You do not have enough leave days remaining')
       return
     }
